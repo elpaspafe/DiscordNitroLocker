cbd72de58382ca587f46e8402bf31e8ef545b8e0
Jose Martínez Granado <86828009+elpaspafe@users.noreply.github.com>
2022-12-08 11:25:57 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,10 @@ git clone https://github.com/logicguy1/Discord-Nitro-Generator-and-Checker.git
 
 cd ./Discord-Nitro-Generator-and-Checker
 
+su root
+
+apt install pip
+
 python3.8 -m pip install -r requirements.txt
 
-python main.py
+python3 main.py
