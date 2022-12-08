54213e4b1a5127b645ccaffb2d491427815f2a65
Jose Martínez Granado <86828009+elpaspafe@users.noreply.github.com>
2022-12-08 11:01:47 +0100

    .sh script

---
+++ start.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/logicguy1/Discord-Nitro-Generator-and-Checker.git
+
+cd ./Discord-Nitro-Generator-and-Checker
+
+python3.8 -m pip install -r requirements.txt
+
+python main.py
